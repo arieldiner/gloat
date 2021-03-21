712d928ccc03acf33418e476f508818b1055dc99
arieldiner <79484510+arieldiner@users.noreply.github.com>
2021-03-21 10:09:10 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 #0. cleanup
 docker rm -f $(docker ps -a -q)
 sudo rm -rf /var/jenkins_home/workspace/tsunami-scanner
-
+sudo rm -rf /var/jenkins_home/workspace/gloat
 #1. clone and build tsunami image
 git clone https://github.com/google/tsunami-security-scanner.git
 cd tsunami-security-scanner
@@ -21,20 +21,25 @@ docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' unvu
 sudo cp /tmp/servers.list /var/jenkins_home/workspace/tsunami-scanner/servers.list
 cd ..
 
-#4. clone my repo
+#5. clone my repo
 git clone https://github.com/arieldiner/gloat.git
 cd gloat/jcasc
 
-#5. replace localhost with IP
+#6. replace localhost with IP
 sed -i "s/localhost/$(hostname -I | awk '{print $1}')/g" casc.yaml
 
-#6. build jenkins as code
+#7. inject ip list as input parameter
+ip_list=$(paste -d, -s /tmp/servers.list)
+sed -i "s/10.10.10.10/$ip_list/g" casc.yaml
+
+
+#8. build jenkins as code
 docker build -t jenkins:jcasc .
 
-#7. run smtp serevr
+#9. run smtp serevr
 docker run -d -p 8025:8025 -p 1025:1025 mailhog/mailhog
 
-#8. run jenkins as code
+#10. run jenkins as code
 
 sudo docker run --name jenkins -u root -v $(which docker):/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock -v /var/jenkins_home/workspace/tsunami-scanner:/var/jenkins_home/workspace/tsunami-scanner --rm -p 8080:8080 --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=password jenkins:jcasc
 
